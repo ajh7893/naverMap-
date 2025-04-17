c5d2d394aed87cb52dd570695744dc209fe69a8f
ajh <jhan@ntro.kr>
2025-04-17 16:06:27 +0900

    소스 업데이트 : 범례추가 및 지도 업데이트

---
+++ src/main/resources/static/common/js/ChartCommon.js
@@ -0,0 +1,60 @@
+var jsonObject = [
+	{"UUID":"e1ba0643-260c-4708-a544-d66571302523","inputDt":"2019-11-20 10:05:44.386128","registerNm":"","updtDt":"","updusrNm":"","fctSe":"수도꼭지 일반","inspctYear":2019,"inspctMt":1,"suplyFltpltNm":"광교정수장","fctLc":"경기도 수원시","colctDe":"2019-01-14","ttclnes":"0","ttclfms":"불검출","fcclfms":"불검출","nh3n":"0","cu":"0","zn":"0.007","cl":"20.4","fe":"0","mn":"0","recl":"0.40"},
+	{"UUID":"aee259f0-c85d-4a11-9069-540402ce0e2c","inputDt":"2019-11-25 13:05:10.740545","registerNm":"","updtDt":"","updusrNm":"","fctSe":"수도꼭지 일반","inspctYear":2019,"inspctMt":2,"suplyFltpltNm":"광교정수장","fctLc":"경기도 수원시","colctDe":"2019-01-14","ttclnes":"3","ttclfms":"불검출","fcclfms":"불검출","nh3n":"0","cu":"0","zn":"0.007","cl":"21.4","fe":"0","mn":"0","recl":"0.52"},
+	{"UUID":"4e1ca429-81d5-4034-91be-4d30d8913d4c","inputDt":"2019-11-28 09:48:36.202669","registerNm":"","updtDt":"","updusrNm":"","fctSe":"수도꼭지 일반","inspctYear":2019,"inspctMt":2,"suplyFltpltNm":"판교정수장","fctLc":"경기도 성남시","colctDe":"2019-01-14","ttclnes":"0","ttclfms":"불검출","fcclfms":"검출","nh3n":"0","cu":"0","zn":"0.011","cl":"19.5","fe":"0","mn":"0","recl":"0.60"},
+	{"UUID":"1947ea63-2996-4f7c-8ca0-a61c3ec3c751","inputDt":"2019-12-02 15:14:05.134621","registerNm":"","updtDt":"","updusrNm":"","fctSe":"수도꼭지 일반","inspctYear":2019,"inspctMt":1,"suplyFltpltNm":"광교정수장","fctLc":"경기도 수원시","colctDe":"2019-01-14","ttclnes":"0","ttclfms":"불검출","fcclfms":"불검출","nh3n":"0","cu":"0","zn":"0.007","cl":"20.4","fe":"0","mn":"0","recl":"0.40"},
+	{"UUID":"5863ad94-5e00-4560-8074-d7eb3dc706c9","inputDt":"2019-12-02 15:14:05.134621","registerNm":"","updtDt":"","updusrNm":"","fctSe":"수도꼭지 노후","inspctYear":2019,"inspctMt":2,"suplyFltpltNm":"비산정수장","fctLc":"경기도 안양시","colctDe":"2019-02-16","ttclnes":"0","ttclfms":"불검출","fcclfms":"불검출","nh3n":"0","cu":"0","zn":"0.002","cl":"19.2","fe":"0","mn":"0","recl":"0.65"},
+	{"UUID":"09aafed7-9324-4351-aa2d-f383e6b2f962","inputDt":"2019-12-02 15:14:05.134621","registerNm":"","updtDt":"","updusrNm":"","fctSe":"수도꼭지 노후","inspctYear":2019,"inspctMt":2,"suplyFltpltNm":"광교정수장","fctLc":"경기도 수원시","colctDe":"2019-02-16","ttclnes":"0","ttclfms":"불검출","fcclfms":"불검출","nh3n":"0","cu":"0","zn":"0.003","cl":"20.0","fe":"0","mn":"0","recl":"0.40"},
+	{"UUID":"cd5b608e-3e45-40cf-bb3e-809053ded38e","inputDt":"2019-12-02 15:14:05.134621","registerNm":"","updtDt":"","updusrNm":"","fctSe":"수도꼭지 일반","inspctYear":2019,"inspctMt":3,"suplyFltpltNm":"광교정수장","fctLc":"경기도 수원시","colctDe":"2019-03-15","ttclnes":"0","ttclfms":"불검출","fcclfms":"불검출","nh3n":"0","cu":"0","zn":"0.004","cl":"20.1","fe":"0","mn":"0","recl":"0.67"},
+	{"UUID":"a2f833be-9b4e-47d8-9f50-9b714db0a6f3","inputDt":"2019-12-02 15:14:05.134621","registerNm":"","updtDt":"","updusrNm":"","fctSe":"수도꼭지 노후","inspctYear":2019,"inspctMt":4,"suplyFltpltNm":"광교정수장","fctLc":"경기도 수원시","colctDe":"2019-04-17","ttclnes":"0","ttclfms":"불검출","fcclfms":"불검출","nh3n":"0","cu":"0","zn":"0.005","cl":"28.9","fe":"0","mn":"0","recl":"0.27"},
+	{"UUID":"4bae8c1b-39fe-4f23-9b9f-d3e396ce4ff5","inputDt":"2019-12-02 15:14:05.134621","registerNm":"","updtDt":"","updusrNm":"","fctSe":"수도꼭지 일반","inspctYear":2019,"inspctMt":5,"suplyFltpltNm":"비산정수장","fctLc":"경기도 안양시","colctDe":"2019-05-03","ttclnes":"0","ttclfms":"불검출","fcclfms":"불검출","nh3n":"0","cu":"0","zn":"0.006","cl":"30.8","fe":"0","mn":"0","recl":"0.37"},
+	{"UUID":"2d7d4c3b-1cf4-4e5d-8e4c-f161cb0d9005","inputDt":"2019-12-02 15:14:05.134621","registerNm":"","updtDt":"","updusrNm":"","fctSe":"수도꼭지 노후","inspctYear":2019,"inspctMt":5,"suplyFltpltNm":"광교정수장","fctLc":"경기도 수원시","colctDe":"2019-05-03","ttclnes":"0","ttclfms":"불검출","fcclfms":"불검출","nh3n":"0","cu":"0","zn":"0.007","cl":"14.7","fe":"0","mn":"0","recl":"0.44"}
+];
+//var jsonKeys=["nh3n","cu","zn","cl","fe","mn","recl"];
+//var jsonNames=["암모니아성질소","동","아연","염소이온","철","망간","잔류염소"];
+var jsonKeys=["nh3n","cu","zn","fe","mn","recl"];
+var jsonNames=["암모니아성질소","동","아연","철","망간","잔류염소"];
+/*
+$(document).ready(function(){
+
+	drawChart('chart01', 'line', jsonObject, jsonKeys, jsonNames);
+});
+*/
+function drawChart(div, type, chartName ,json, jsonKeys, jsonNames){
+	var to = '#'+div;
+	var i = 0;
+	while(i < jsonKeys.length){
+		//jsonObject = jsonObject.replace(jsonKeys[i],jsonNames[i]);
+		json = JSON.parse(JSON.stringify(json).split("\""+jsonKeys[i]+"\":").join("\""+jsonNames[i]+"\":"));
+		//document.write(JSON.stringify(jsonObject));
+		i++;
+	}
+	var chart = c3.generate({
+	    bindto: to,
+	    data: {
+	      json : json,
+	      keys : {
+	    	  value: jsonNames
+	      },
+	      type: type
+	    },
+	    title:{
+	    	text: chartName,
+	    	postion: 'top-center' //top-left, top-center, top-right
+	    }
+	});
+}
+
+function exportChartToPng(chartID){
+	//fix weird back fill
+	d3.select('#'+chartID).selectAll("path").attr("fill", "none");
+	//fix no axes
+	d3.select('#'+chartID).selectAll("path.domain").attr("stroke", "black");
+	//fix no tick
+	d3.select('#'+chartID).selectAll(".tick line").attr("stroke", "black");
+	d3.select('#'+chartID).selectAll("g text").style("font-weight","bold");
+	
+	var svgElement = $('#'+chartID).find('svg')[0];
+	saveSvgAsPng(svgElement, chartID+'.png');
+}
+
--- src/main/resources/static/common/js/customScript.js
+++ src/main/resources/static/common/js/customScript.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
     //GNB 임시 스크립트
     $("#nav").on("mouseenter focus", function () {
         $("#nav").addClass("active");
@@ -12,19 +13,6 @@ $(document).ready(function () {
         $(".gnb_bg").removeClass("active");
     });
 
-  //상단 검색 토글버튼
-  $("#topSearchBtn").click(function () {
-    $(this).toggleClass("active");
-    $("#searchArea").toggleClass("active");
-    $(".mask").toggleClass("active");
-  });
-
-    //상단 검색 토글버튼
-    $("#topSearchBtn").click(function(){
-      $(this).toggleClass("active");
-      $("#searchArea").toggleClass("active");
-      $(".mask").toggleClass("active");
-    });
 
     //상단 검색 토글버튼
     $("#topSearchBtn").click(function(){
@@ -32,32 +20,6 @@ $(document).ready(function () {
       $("#searchArea").toggleClass("active");
       //$(".mask").toggleClass("active");
     });
-
-    //상단 검색 토글버튼
-    $("#topSearchBtn").click(function(){
-      $(this).toggleClass("active");
-      $("#searchArea").toggleClass("active");
-      //$(".mask").toggleClass("active");
-    });
-
-  //상단 검색 토글버튼
-  $("#topSearchBtn").click(function () {
-    $(this).toggleClass("active");
-    $("#searchArea").toggleClass("active");
-    //$(".mask").toggleClass("active");
-  });
-
-  //상단 검색 토글버튼
-  $("#topSearchBtn").click(function () {
-    $(this).toggleClass("active");
-    $("#searchArea").toggleClass("active");
-    $(".mask").toggleClass("active");
-
-  //상단 검색 토글버튼
-  $("#topSearchBtn").click(function () {
-    $(this).toggleClass("active");
-    $("#searchArea").toggleClass("active");
-    $(".mask").toggleClass("active");
 
 
     //모바일 메뉴 토글버튼
@@ -65,9 +27,29 @@ $(document).ready(function () {
       $(this).toggleClass("active");
       $("body").toggleClass("fixed");
       $("#mobileNav").toggleClass("active");
+    });
 
-  });
+
+    // 모바일 GNB 작동
+    var mNavList = document.querySelectorAll('#mobileNav > ul > li');
+  
+    for(var i = 0; i < mNavList.length; i++){
+      mNavList[i].querySelector('.depth1 > a').addEventListener('click', function(e){
+        e.preventDefault();
+        for(var j = 0; j < mNavList.length; j++){
+          mNavList[j].classList.remove('on');
+        }
+        this.parentNode.classList.add('on');
+      });
+    }
+
+
+    $(".subnav > ul > li > a").click(function(){
+			$(this).parent().toggleClass("on");
+		});
+
 	
+
     //메인 슬라이드
     var $status = $('.custom_paging'); //paging 이 들어갈 div 혹은 p 혹은 어떠한 영역
     var $slickElement = $('.main_slide'); //slide가 되어야하는 div
@@ -85,14 +67,6 @@ $(document).ready(function () {
         dots: false,
     })
 
-  //메인 슬라이드
-  var $status = $(".custom_paging"); //paging 이 들어갈 div 혹은 p 혹은 어떠한 영역
-  var $slickElement = $(".main_slide"); //slide가 되어야하는 div
-
-  //메인 슬라이드
-  var $status = $('.custom_paging'); //paging 이 들어갈 div 혹은 p 혹은 어떠한 영역
-  var $slickElement = $('.main_slide'); //slide가 되어야하는 div
-
    // 메인 슬라이드 일시정지 기능
     $(document).on("click", ".btn_pause", function(){
         $(".main_slide").slick("slickPause");
@@ -100,36 +74,6 @@ $(document).ready(function () {
         $(this).parent().attr("title", "재생");
     });
 
-  $slickElement.on(
-    "init reInit afterChange",
-    function (event, slick, currentSlide, nextSlide) {
-      var i = (currentSlide ? currentSlide : 0) + 1;
-      $status.text(i + " / " + slick.slideCount);
-    }
-  );
-
-  $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-    var i = (currentSlide ? currentSlide : 0) + 1;
-    $status.text(i + ' / ' + slick.slideCount);
-  });
-
-
-
-  $slickElement.slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    dots: false,
-  });
-
-  $slickElement.slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    dots: false,
-  })
 
     // 메인 슬라이드 재생 기능
     $(document).on("click", ".btn_play", function(){
@@ -138,16 +82,15 @@ $(document).ready(function () {
         $(this).parent().attr("title", "일시정지");
     });
 
+
     // 팝업 슬라이드
     var $popStatus = $('.pop_custom_paging');
     var $popSlickElement = $('.popSlider');
-
 
     $popSlickElement.on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
         var i = (currentSlide ? currentSlide : 0) + 1;
         $popStatus.text(i + ' / ' + slick.slideCount);
     });
-
 
     $popSlickElement.slick({
         slidesToShow: 1,
@@ -163,7 +106,7 @@ $(document).ready(function () {
 
     tabItem.forEach((item, index) => {
       item.addEventListener("click", (e) => {
-      e.preventDefault(); // a
+      e.preventDefault(); // a 
       
       tabContent.forEach((content) => {
         content.classList.remove("active");
@@ -178,5 +121,68 @@ $(document).ready(function () {
       });
     });
     
+    
+
+    // 팔당
+    $('.tour_slide').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: false,
+      infinite: false,
+      responsive: [
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: false,
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: false,
+          }
+        },
+      ]
+    });
+
+
+    //
+    $('.eco_wrap button').click(function(){
+      if($(this).is('.on')){
+        $('.eco_wrap .eco_drop').hide()
+      } else {
+        $('.eco_wrap .eco_drop').show()
+      }
+    })
+    
+    $(".eco_drop li").on("click", function(e){
+      colorChangeOnClick(e);
+      $('.eco_wrap button').text('')
+      $('.eco_wrap button').text($(this).text())
+
+      $(".fade").css('display','none');
+
+      if(!$('.eco_wrap button').is(':hidden')){
+        $('.eco_wrap .eco_drop').hide()
+      }
+    });
+
+      
 
 });
+
+  
+//
+function colorChangeOnClick(e){
+	var curLi = e.currentTarget;
+	var curLiClass = curLi.classList.value;
+	if(curLiClass != "active"){
+		$(".eco_drop li").removeClass('active');
+		curLi.classList.add('active');
+
+	}
+}
